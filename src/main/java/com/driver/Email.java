361e4e2634dba9da47c43dd7925028196ef331f4
unknown <nagendra.poojary112@gmail.com>
2023-02-18 21:47:09 +0530

    commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -20,6 +20,24 @@ public class Email {
 
     public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        int upperCount = 0;
+        int lowerCount = 0;
+        int digitCount = 0;
+        if(oldPassword.equals(this.password) && newPassword.length()>=8){
+            //System.out.println(getPassword());
+            for(int i=0; i<newPassword.length(); i++){
+                char ch = newPassword.charAt(i);
+                if(Character.isUpperCase(ch))
+                    upperCount++;
+                else if(Character.isLowerCase(ch))
+                    lowerCount++;
+                else if(Character.isDigit(ch))
+                    digitCount++;
+            }
+            if(upperCount>0 && lowerCount>0 && digitCount>0 && (upperCount+lowerCount+digitCount) < newPassword.length()){
+                this.password = newPassword;
+            }
+        }
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
